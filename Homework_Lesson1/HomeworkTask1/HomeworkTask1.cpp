46814ab51361bc0110f0000a4d852e2ea8a4b454
Igor Spiridonov <stormdemons@mail.ru>
2024-11-11 16:03:10 +0300

    Добавлены в отслеживаемые все 3 задачи

---
+++ Homework_Lesson1/HomeworkTask1/HomeworkTask1.cpp
@@ -0,0 +1,99 @@
+﻿
+#include <iostream>
+using namespace std;
+
+enum month {
+    январь=1,
+    февраль,
+    март,
+    апрель,
+    май,
+    июнь,
+    июль,
+    август,
+    сентябрь,
+    октябрь,
+    ноябрь,
+    декабрь
+};
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    int num;
+
+    cout << "Введите номер месяца: ";
+    cin >> num;
+    while (num != 0) {
+        switch (num)
+        {
+        case static_cast<int>(month::август):
+                cout << "Август" << endl;
+                cout << "Введите номер месяца: ";
+                cin >> num;
+                break;
+        case static_cast<int>(month::январь):
+            cout << "Январь" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::февраль):
+            cout << "февраль" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::март):
+            cout << "март" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::апрель):
+            cout << "апрель" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::май):
+            cout << "май" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::июнь):
+            cout << "июнь" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::июль):
+            cout << "июль" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::сентябрь):
+            cout << "сентябрь" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::октябрь):
+            cout << "октябрь" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::ноябрь):
+            cout << "ноябрь" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        case static_cast<int>(month::декабрь):
+            cout << "декабрь" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        default:
+            cout << "Неправильный номер!" << endl;
+            cout << "Введите номер месяца: ";
+            cin >> num;
+            break;
+        }
+    }
+    cout << "До свидания" << endl;
+}
+
---
+++ Homework_Lesson1/HomewrokTask3/HomewrokTask3.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+using namespace std;
+
+struct Adress {
+    string city;
+    string ylica;
+    int num_home;
+    int num_kv;
+    int index;
+};
+
+void print_adress(Adress gorod) {
+    cout << "Город: " << gorod.city << endl;
+    cout << "Улица: "<< gorod.ylica << endl;
+    cout << "Номер дома: "<< gorod.num_home << endl;
+    cout << "Номер квартиры: "<< gorod.num_kv << endl;
+    cout << "Индекс: "<< gorod.index << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    Adress first,first2;
+    
+first.city = "Москва";
+
+first.ylica= "Арбат";
+
+first.num_home= 12;
+
+first.num_kv= 8;
+
+first.index= 123456;
+
+print_adress(first);
+
+first2.city = "Ижевск";
+
+first2.ylica = "Пушкина";
+
+first2.num_home = 59;
+
+first2.num_kv = 143;
+
+first2.index = 953769;
+
+print_adress(first2);
+
+}
+
