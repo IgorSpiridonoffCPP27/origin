66be4ca3f42a7ec10c640958392496f50b0b3674
Igor Spiridonov <stormdemons@mail.ru>
2024-12-02 15:12:33 +0300

    add Homework4

---
+++ Homework_Lesson4/Homework_Task1/Homework_Task1.cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+class Figure {
+protected:
+    int info=0;
+    string name = "Фигура";
+public:
+    virtual void get_info() {
+        cout << name << ":" << info << endl;
+    }
+
+
+};
+
+class Trinangle : public Figure {
+public:
+    Trinangle(int a) {
+        info = a;
+        name = "Треугольник";
+    }
+};
+
+class Chetirehegolnic : public Figure {
+public:
+    Chetirehegolnic(int a) {
+        info = a;
+        name = "Четырехугольник";
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Figure f;
+    Trinangle T(3);
+    Chetirehegolnic C(4);
+    f.get_info();
+    T.get_info();
+    C.get_info();
+}
