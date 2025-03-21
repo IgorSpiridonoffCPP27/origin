168bfa42905bf97f33b18f0ff13e107b15b35910
Igor Spiridonov <stormdemons@mail.ru>
2025-03-21 23:01:28 +0300

    Homework_Lesson6

---
+++ Homework_Lesson6_STL. часть 2/Homework_Task1/Homework_Task1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <exception>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+
+	vector<int> arr = { 1,1 ,2 ,5 ,6, 1, 2, 4 };
+	sort(arr.begin(),arr.end());
+	auto it = unique(arr.begin(), arr.end());
+	arr.erase(it, arr.end());
+	for (auto el : arr) {
+		cout << el<< " ";
+	}
+
+	return 0;
+}
